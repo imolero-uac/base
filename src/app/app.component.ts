9a5f240b1a5ce945c1fe31f6facca7e2ae69fdfc
Ivan Molero <imolero@uandina.edu.pe>
2021-09-13 23:39:02 -0500

    avance 20210913

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,62 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'base';
 }
+
+console.log('Bienvenidos a Typescript');
+
+
+//#region variables
+// let nombre: string = 'Arturo';
+// let ahorros: number | string = 500;
+// let esAlto: boolean | string = true;
+// esAlto = false;
+// esAlto = "sin información";
+// ahorros  = 1000;
+// ahorros = 'sin ahorros';
+// console.log(nombre, ahorros, esAlto);
+//#endregion
+
+
+//#region arreglos interfaces objetos
+// let pasatiempos: (string | number)[] = ['caminar', 'violin'];
+
+// pasatiempos.push('ajedrez');
+// pasatiempos.push(5);
+// console.log(pasatiempos);
+
+// const edad: number[] = [5, 6, 5];
+// //edad = [6,6,6,6];
+// edad.push(10);
+// edad.pop();
+// edad.pop();
+// console.log(edad);
+
+// interface Estudiante {
+//   nombre: string,
+//   apPaterno: string,
+//   codigo: string,
+//   pension: number,
+//   estaMatriculado: boolean,
+//   pasatiempos: string[],
+//   escuela?: string
+// }
+
+// let alumno: Estudiante = {
+// nombre: 'Juan',
+// apPaterno: 'Garcia',
+// codigo: '000951554j',
+// pension: 850,
+// estaMatriculado: true,
+// pasatiempos: ['futbol', 'basket']
+// };
+
+// alumno.escuela = 'Ingeniería de Sistemas';
+
+// console.log(alumno);
+// console.table(alumno);
+//#endregion
+
+
+
+
+
